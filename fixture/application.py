a0325c9dbd653f1de22d98a544af9f91b0bbb153
as1ww <n45.suhanova@yandex.ru>
2022-11-20 13:43:37 +0300

    Получение почты по протоколу POP3

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 class Application:
@@ -17,6 +19,8 @@ class Application:
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.config = config
         self.base_url = config['web']['baseUrl']
 
